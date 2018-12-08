55cd9e9940eaf13dfc5b1f207102203ee261f185
Jochen Boesmans <jochen.boesmans@gmail.com>
2018-12-08 13:18:46 +0100

    another heroku try

--- src/websocketclient/index.js
+++ src/websocketclient/index.js
@@ -3,7 +3,9 @@ import {updateMarket} from "../actions";
 import {store} from "../index";
 
 export const subscribeToSocketBroadcasts = () => {
-  const socket = io();
+  const url = (process.env.NODE_ENV === "production") ? "https://dexaggregate-server.herokuapp.com" : "http://localhost:5000";
+
+  const socket = io(url);
   socket.on("marketBroadcast", receivedMarket => {
     updateMarket(receivedMarket)(store.dispatch);
     console.log(receivedMarket);
