d17069787e2e5a54f5fc10ba36fad3e9133d3bcb
Jochen Boesmans <jochen.boesmans@gmail.com>
2018-12-08 15:31:28 +0100

    .

--- src/websocketclient/index.js
+++ src/websocketclient/index.js
@@ -6,7 +6,7 @@ export const subscribeToSocketBroadcasts = () => {
   const url = "https://dexaggregate-server.herokuapp.com";
 
 
-  const socket = io.connect(":80");
+  const socket = io.connect();
   socket.on("marketBroadcast", receivedMarket => {
     updateMarket(receivedMarket)(store.dispatch);
     console.log(receivedMarket);
