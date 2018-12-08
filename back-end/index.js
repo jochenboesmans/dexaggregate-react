1398327ea331c8ea6dddba8209af5229b541f8d3
Jochen Boesmans <jochen.boesmans@gmail.com>
2018-12-08 07:54:27 +0100

    heroku 3

--- back-end/index.js
+++ back-end/index.js
@@ -8,7 +8,7 @@ const config = require("./config");
 // TODO: Maybe use this in production
 if (process.env.NODE_ENV === "production") {
 	// Make Express serve front-end assets in react build
-	app.use(express.static('client/build'));
+	app.use(express.static('../build'));
 
 
 	const path = require('path');
@@ -20,13 +20,13 @@ if (process.env.NODE_ENV === "production") {
 //require("./routes/exchanges")(app);
 //require("./routes/market")(app);
 
-const wsPort = process.env.PORT || 5000;
-server.listen(wsPort, () =>  console.log(`WS server is now listening on port ${wsPort}`));
+
 require("./websocketbroadcasts/broadcastSession")(server);
 
 require("./model/periodicallyUpdate")();
 
 //const apiPort = process.env.PORT || 5000;
-
+const wsPort = process.env.PORT || 5000;
 
 //apiServer.listen(apiPort, () => console.log(`API server is now listening on port ${apiPort}`));
+server.listen(wsPort, () =>  console.log(`WS server is now listening on port ${wsPort}`));
---
+++ build/precache-manifest.2a5897eb61da38dd34ce2c8d0bc4dd3c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c95d77c2ad394812f92e",
+    "url": "/static/js/main.c95d77c2.chunk.js"
+  },
+  {
+    "revision": "b873692c25b329ef9cb5",
+    "url": "/static/js/1.b873692c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bfaa91f55d1df9063b38602507c2bdfa",
+    "url": "/index.html"
+  }
+];
