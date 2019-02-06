adad641eb9a1ac47d3e2afb460fc26efbf57eaff
Jochen Boesmans <jochen.boesmans@gmail.com>
2019-02-06 19:01:21 +0100

    market model now updates frequently.

--- model/periodicallyUpdate.js
+++ model/periodicallyUpdate.js
@@ -7,26 +7,27 @@ const projectID = `3623107a49ce48a9b3687ec820e8a222`;
 const web3 = new Web3(`wss://mainnet.infura.io/ws/v3/${projectID}`);
 
 const tryUpdateExchangeMarkets = require("./exchangemarkets/tryUpdateExchangeMarkets");
-let lastUpdate = 0;
+/*let lastUpdate = 0;
+
+let subscription;*/
 
 module.exports = async () => {
-	if (Date.now() - lastUpdate >= 10 * 1000) {
-		tryUpdateExchangeMarkets(web3);
-		lastUpdate = Date.now();
-	}
+	tryUpdateExchangeMarkets(web3);
+	setInterval(() => tryUpdateExchangeMarkets(web3), 5 * 1000);
 
-	web3.eth.clearSubscriptions();
-
-	const subscription = web3.eth.subscribe("newBlockHeaders", (error, result) => {
-		error ? console.log(error) : console.log(result);
-	});
-
-	subscription.on("data", (blockHeader) => {
+	/*subscription.on("data", (blockHeader) => {
 		console.log("new data");
 		if (Date.now() - lastUpdate >= 10 * 1000) {
 			tryUpdateExchangeMarkets(web3);
 			lastUpdate = Date.now();
 		}
-	});
+	});*/
 
 };
+
+/*const renewSubscription = () => {
+	web3.eth.clearSubscriptions();
+	subscription = web3.eth.subscribe("newBlockHeaders", (error, result) => {
+		error ? console.log(error) : console.log(result);
+	});
+};*/
