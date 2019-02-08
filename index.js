63514bf579b03a5a2c114d442416de99c9c14890
Jochen Boesmans <jochen.boesmans@gmail.com>
2019-02-08 14:52:33 +0100

    Outcommented Express code to test in production.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const express = require("express");
-const app = express();
-const server = require("http").createServer(app);
+/*const express = require("express");
+const app = express();*/
+const server = require("http").createServer();
 
-const config = require("./config");
-
-if(process.env.NODE_ENV === "production") {
+/*if(process.env.NODE_ENV === "production") {
 	// Make Express serve front-end assets in react build
 	app.use(express.static("client/build"));
 
@@ -12,12 +10,12 @@ if(process.env.NODE_ENV === "production") {
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
-}
+}*/
 
 require("./model/periodicallyUpdate")();
 
-require("./routes/exchanges")(app);
-require("./routes/market")(app);
+/*require("./routes/exchanges")(app);
+require("./routes/market")(app);*/
 
 require("./websocketbroadcasts/broadcastSession")(server);
 
