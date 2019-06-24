a3cc13915c127e358024d8ec873423c1d7c57f4c
Jochen Boesmans <jochen.boesmans@gmail.com>
2019-06-24 08:55:02 +0200

    Remove installation of MUI alpha.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 
-import installMUIstyles from "@material-ui/styles/install";
-
 /* No lazy initialization here because component is always loaded. */
 import GlobalContextProvider from "./components/GlobalContextProvider";
-
-/* Temporary solution for usage of alpha version of MUI styles */
-installMUIstyles();
 
 render(<GlobalContextProvider/>, document.getElementById(`root`));
