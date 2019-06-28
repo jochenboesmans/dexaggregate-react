3e9f7838642b6b795c2c66b51019cbeacb728972
Jochen Boesmans <jochen.boesmans@gmail.com>
2019-06-28 12:01:11 +0200

    Improved price format.

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ import { render } from "react-dom";
 /* No lazy initialization here because component is always loaded. */
 import GlobalContextProvider from "./components/GlobalContextProvider";
 
-render(createElement(GlobalContextProvider), document.getElementById(`root`));
+render(createElement(GlobalContextProvider), document.getElementById("root"));
--- src/util/format.ts
+++ src/util/format.ts
@@ -1,46 +1,26 @@
-const formatPrice = (price) => {
-	const sd = (price) => {
-		if (price < 0.01) {
-			return 1;
-		} else if (price < 0.1) {
-			return 2;
-		} else if (price < 1) {
-			return 3;
-		} else {
-			return 4;
-		}
-	};
-	if (price < 0.001) {
-		return (new Intl.NumberFormat(`en-US`, {
-			style: `currency`,
-			currency: `USD`,
-			maximumFractionDigits: 3,
-			minimumFractionDigits: 3,
-			useGrouping: true
-		}).format(price));
-	} else {
-		return (new Intl.NumberFormat(`en-US`, {
-			style: `currency`,
-			currency: `USD`,
-			minimumSignificantDigits: sd(price),
-			maximumSignificantDigits: sd(price),
-			useGrouping: true
-		}).format(price));
-	}
+const amountOfDigits = (x: number) => (Math.log10((x ^ (x >> 31)) - (x >> 31)) | 0) + 1;
+
+const formatPrice = (price: number) => {
+	const sd = (price: number) => amountOfDigits(price / 0.01);
+	return (new Intl.NumberFormat("en-US", {
+		style: "currency",
+		currency: "USD",
+		minimumSignificantDigits: sd(price),
+		maximumSignificantDigits: sd(price),
+		useGrouping: true
+	}).format(price));
 };
 
-const formatVolume = (volume) => {
-	return (new Intl.NumberFormat(`en-US`, {
-		style: `currency`, currency: `USD`, useGrouping: true
+const formatVolume = (volume: number) => {
+	return (new Intl.NumberFormat("en-US", {
+		style: "currency", currency: "USD", useGrouping: true
 	}).format(volume));
 };
 
-const formatPercentage = (percentage) => {
-	return (new Intl.NumberFormat(`en-US`, {
-		style: `percent`, useGrouping: true, minimumFractionDigits: 2, maximumFractionDigits: 2
+const formatPercentage = (percentage: number) => {
+	return (new Intl.NumberFormat("en-US", {
+		style: "percent", useGrouping: true, minimumFractionDigits: 2, maximumFractionDigits: 2
 	}).format(percentage));
 };
 
-export {
-	formatPrice, formatVolume, formatPercentage,
-};
+export { formatPrice, formatVolume, formatPercentage };
